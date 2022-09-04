b4130c188f5d5918fa759b0f153630cc8a261882
vinlenka <2371607v@student.gla.ac.uk>
2022-09-04 13:32:55 +0200

    joining with face.py

--- recommend.py
+++ recommend.py
@@ -3,7 +3,7 @@ import random
 import authorization # this is the script we created earlier
 import numpy as np
 from numpy.linalg import norm
-
+from face import get_VAD
 
 def recommend(mood_vec, ref_df, n_recs=5):
     track_moodvec = mood_vec
@@ -21,8 +21,12 @@ df = pd.read_csv("muse_v3.csv")
 df["mood_vec"] = df[["valence_tags","arousal_tags", "dominance_tags"]].values.tolist()
 
 # Valence, arousal, dominance
-mood_vec = [1.1338692880875, 6.037996638576, 2.4078040275840005]
-result = (recommend(mood_vec, ref_df = df, n_recs = 5))
+mood_vec = get_VAD("../data/fear3.jpg")  # [1.1338692880875, 6.037996638576, 2.4078040275840005]
+result = (recommend(mood_vec, ref_df = df, n_recs = 15))
 
 # print(result[['track','spotify_id']])
-print(np.array(result[['lastfm_url', 'track', 'spotify_id', 'artist','distances']]))
+# print(np.array(result[['lastfm_url', 'track', 'spotify_id', 'artist','distances']]))
+
+final_results = result.dropna().iloc[0]['spotify_id']
+output = "https://open.spotify.com/track/" #67rLkvNELkXfa64EdpYT1A"
+print(output+final_results)
